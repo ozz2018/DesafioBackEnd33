faa6d14f05d1356d48cbd7bb1565557b25a9ac6f
Oskar Solano <157350576+ozz2018@users.noreply.github.com>
2024-06-09 21:55:46 -0600

    Update users.usecase.js

--- src/usecases/users.usecase.js
+++ src/usecases/users.usecase.js
@@ -1,3 +1,11 @@
 const createError = require("http-errors");
 const encrypt = require("../lib/encrypt");
 const Users = require("../models/users.models");
+
+async function create(userData) {
+    const userFound = await Users.findOne({ email: userData.email });
+    if (userFound) throw createError(409, "Email already in use");
+    userData.password = await encrypt.encrypt(userData.password);
+    const newUser = await Users.create(userData);
+    return newUser;
+}
