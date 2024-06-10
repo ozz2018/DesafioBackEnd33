5e878f57563d29e2574280bd168af2bd6d8c420d
Oskar Solano <157350576+ozz2018@users.noreply.github.com>
2024-06-09 21:53:01 -0600

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,2 +1,22 @@
 const cors = require("cors");
 const express = require("express");
+const usersRouter = require("./routers/users.routes");
+const authRouter = require("./routers/auth.routes");
+const postsRouter = require("./routers/posts.routes");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/user", usersRouter);
+app.use("/auth", authRouter);
+app.use("/post", postsRouter);
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Desafío Backend APIv1",
+  });
+});
+
+module.exports = app;
