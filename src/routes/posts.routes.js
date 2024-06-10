095ed4f8ae5258d5f7a1a13b50b2f4bd641562c7
Oskar Solano <157350576+ozz2018@users.noreply.github.com>
2024-06-09 22:30:39 -0600

    Update posts.routes.js

--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -2,3 +2,24 @@ const express = require("express");
 const postsUseCase = require("../usecases/posts.usecase");
 const auth = require("../middlewares/auth.middleware");
 const jwt = require("../lib/jwt");
+
+
+const route = express.Router();
+
+route.post("/", auth, async (req, res) => {
+  try {
+    req.body.user = req.user.id;
+    const posts = await postsUseCase.create(req.body);
+
+    res.json({
+      succes: true,
+      data: { post: posts },
+    });
+  } catch (error) {
+    res.status(error.status || 500);
+    res.json({
+      succes: false,
+      error: error.message,
+    });
+  }
+});
