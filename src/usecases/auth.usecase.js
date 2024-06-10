55887180a36263abd276e2855845fcc212b34ea0
Oskar Solano <157350576+ozz2018@users.noreply.github.com>
2024-06-09 22:20:29 -0600

    Update auth.usecase.js

--- src/usecases/auth.usecase.js
+++ src/usecases/auth.usecase.js
@@ -1,5 +1,5 @@
 const createError = require("http-errors");
-const Users = require("../models/users.models");
+const Users = require("../models/users.models")
 const jwt = require("../lib/jwt");
 const encrypt = require("../lib/encrypt");
 
