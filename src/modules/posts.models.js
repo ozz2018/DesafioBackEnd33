4b43e0b36a0fa4d78f1c9faf2f904d4870c3085c
Oskar Solano <157350576+ozz2018@users.noreply.github.com>
2024-06-09 22:29:13 -0600

    update

--- src/modules/posts.models.js
+++ src/modules/posts.models.js
@@ -1,35 +1,35 @@
-const mongoose = require("mongoose");
-const modelName = "posts";
+const mongoose = require("mongoose")
+const modelName = "posts"
 const schema = new mongoose.Schema({
     title: {
-    type: String,
-    required: true,
-    minLength: 1,
-    maxLength: 100,
+        type: String,
+        required: true,
+        minLength: 1,
+        maxLength: 100,
     },
     image: {
-    type: String,
-    required: false,
-    minLength: 10,
-    maxLength: 500,
+        type: String,
+        required: false,
+        minLength: 10,
+        maxLength: 500,
     },
     body: {
-    type: String,
-    required: true,
-    minLength: 1,
-    maxLength: 1000,
+        type: String,
+        required: true,
+        minLength: 1,
+        maxLength: 1000,
     },
     user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "user",
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
     },
     created_at: {
-    type: Date,
-    default: Date.now,
+        type: Date,
+        default: Date.now,
     },
     updated_at: {
-    type: Date,
-    default: Date.now,
+        type: Date,
+        default: Date.now,
     },
 });
 
--- src/modules/users.models.js
+++ src/modules/users.models.js
@@ -2,33 +2,33 @@ const mongoose = require("mongoose")
 const modelName = "user"
 const schema = new mongoose.Schema({
     name: {
-    type: String,
-    required: true,
-    minLength: 2,
-    maxLength: 100,
+        type: String,
+        required: true,
+        minLength: 2,
+        maxLength: 100,
     },
     profilePic: {
-    type: String,
-    required: false,
-    minLength: 10,
-    maxLength: 500,
+        type: String,
+        required: false,
+        minLength: 10,
+        maxLength: 500,
     },
     email: {
-    type: String,
-    required: true,
-    match: /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/,
+        type: String,
+        required: true,
+        match: /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/,
     },
     password: {
-    type: String,
-    required: true,
+        type: String,
+        required: true,
     },
     created_at: {
-    type: Date,
-    default: Date.now,
+        type: Date,
+        default: Date.now,
     },
     updated_at: {
-    type: Date,
-    default: Date.now,
+        type: Date,
+        default: Date.now,
     },
 });
 
