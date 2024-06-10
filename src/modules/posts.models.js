c76fac05a08b711a0d0851fc3809a4c0db696c44
Oskar Solano <157350576+ozz2018@users.noreply.github.com>
2024-06-09 22:12:57 -0600

    Update posts.models.js

---
+++ src/modules/posts.models.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const modelName = "posts";
+
+const schema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    minLength: 1,
+    maxLength: 100,
+  },
+  image: {
+    type: String,
+    required: false,
+    minLength: 10,
+    maxLength: 500,
+  },
+  body: {
+    type: String,
+    required: true,
+    minLength: 1,
+    maxLength: 1000,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+  },
+    created_at: {
+    type: Date,
+    default: Date.now,
+    },
+    updated_at: {
+    type: Date,
+    default: Date.now,
+    },
+});
+
+module.exports = mongoose.model(modelName, schema);
