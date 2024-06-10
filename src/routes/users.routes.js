9d3665111d7069f63f93891b85bd20e828c3b09f
Oskar Solano <157350576+ozz2018@users.noreply.github.com>
2024-06-09 21:59:49 -0600

    Update users.routes.js

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -2,3 +2,19 @@ const express = require("express");
 const usersUseCase = require("../usecases/users.usecase");
 
 const route = express.Router();
+
+}route.post("/", async (req, res) => {
+    try {
+        const user = await usersUseCase.create(req.body);
+        res.json({
+        succes: true,
+        data: { user: user },
+        });
+    } catch (error) {
+        res.status(error.status || 500);
+        res.json({
+        succes: false,
+        error: error.message,
+      });
+    }
+  });
