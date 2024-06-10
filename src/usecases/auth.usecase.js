28bad6e6c63705ca7e0b85b58769e34ba46874ad
Oskar Solano <157350576+ozz2018@users.noreply.github.com>
2024-06-09 22:20:18 -0600

    Update auth.usecase.js

--- src/usecases/auth.usecase.js
+++ src/usecases/auth.usecase.js
@@ -5,19 +5,16 @@ const encrypt = require("../lib/encrypt");
 
 async function login(email, password) {
     const user = await Users.findOne({ email: email });
-  
+
     if (!user) {
-      throw createError(401, "Invalid data");
+        throw createError(401, "Invalid data");
     }
-  
     const isPassValid = await encrypt.compare(password, user.password);
-  
+
     if (!isPassValid) {
-      throw createError(401, "Invalid data");
+        throw createError(401, "Invalid data");
     }
     const token = jwt.sign({ id: user._id });
     return token;
-  }
-  
-  module.exports = { login };
-  
+    }
+module.exports = { login };
