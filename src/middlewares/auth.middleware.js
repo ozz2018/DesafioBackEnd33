11d61809f6723f7d5693732cdc805ea2fd0a0e29
Oskar Solano <157350576+ozz2018@users.noreply.github.com>
2024-06-09 23:01:33 -0600

    Update auth.middleware.js

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,2 +1,26 @@
 const createError = require("http-errors");
 const usersUseCase = require("../usecases/users.usecase");
+
+const jwt = require("../lib/jwt");
+
+async function auth(req, res, next) {
+    try {
+    const token = req.headers.authorization;
+
+    if (!token) {
+        throw createError(401, "JWT is required");
+        }
+        const payload = jwt.verify(token);
+    const user = await usersUseCase.getById(payload.id);
+    req.user = user;
+
+    next();
+} catch (error) {
+        res.status(401);
+        res.json({
+            success: false,
+            error: error.mesage,
+    });
+}
+}
+module.exports = auth
