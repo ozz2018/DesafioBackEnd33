476f3d39c0a8fab6f30dda3b2b4c670963e1ec36
Oskar Solano <157350576+ozz2018@users.noreply.github.com>
2024-06-09 22:24:29 -0600

    Update auth.routes.js

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,22 +1,19 @@
 const express = require("express");
-
 const authUseCase = require("../usecases/auth.usecase");
 
 const router = express.Router();
-
 router.post("/login", async (req, res) => {
     try {
-      const { email, password } = req.body;
-const token = await authUseCase.login(email, password);
-
-res.json({
-        succes: true,
-        data: { token },
+        const { email, password } = req.body;
+        const token = await authUseCase.login(email, password);
+        res.json({
+            succes: true,
+            data: { token },
 });
     } catch (error) {
-    res.status(error.status || 500);
-    res.json({
-        error: error.message,
+        res.status(error.status || 500);
+        res.json({
+            error: error.message,
     });
     }
     });
