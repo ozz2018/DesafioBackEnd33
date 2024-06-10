f5ebcf4470da59c8ddca5a0ccb633ea518860eb2
Oskar Solano <157350576+ozz2018@users.noreply.github.com>
2024-06-09 22:16:09 -0600

    Update encrypt.js

--- src/lib/encrypt.js
+++ src/lib/encrypt.js
@@ -1,16 +1,15 @@
 const bcrypt = require("bcryptjs");
-
 const SALT_ROUNDS = 10;
 
 function encrypt(text) {
-    return bcrypt.hash(text, SALT_ROUNDS);
+    return bcrypt.hash(text, SALT_ROUNDS)
 }
 
 function compare(plainText, hash) {
-    return bcrypt.compare(plainText, hash);
+    return bcrypt.compare(plainText, hash)
 }
 
 module.exports = {
     encrypt,
     compare,
-};
+}
