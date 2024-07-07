310b87de44ef4bfafca13bde48991c5d1dc284f1
Oskar Solano <157350576+ozz2018@users.noreply.github.com>
2024-07-06 23:08:28 -0600

    Update posts.models.js

--- src/models/posts.models.js
+++ src/models/posts.models.js
@@ -1,36 +1,80 @@
-const mongoose = require("mongoose")
-const modelName = "posts"
-const schema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true,
-        minLength: 1,
-        maxLength: 100,
-    },
-    image: {
-        type: String,
-        required: false,
-        minLength: 10,
-        maxLength: 500,
-    },
-    body: {
-        type: String,
-        required: true,
-        minLength: 1,
-        maxLength: 1000,
-    },
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "user",
-    },
-    created_at: {
-        type: Date,
-        default: Date.now,
-    },
-    updated_at: {
-        type: Date,
-        default: Date.now,
-    },
-});
+const { hash } = require('bcryptjs')
+const mongoose = require('mongoose')
 
-module.exports = mongoose.model(modelName, schema)
+const modelName = 'posts'
+
+const postSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true
+  },
+  location: {
+    type: String,
+    default: 'México',
+    required: true
+  },
+  work: {
+    type: String,
+    default: 'Desarrollador',
+    required: true
+  },
+  join: {
+    type: Date,
+    default: Date.now
+  },
+  hashtags: {
+    type: String,
+    default: '#Hola'
+  },
+  hashtags2: {
+    type: String,
+    default: '#Hola'
+  },
+  hashtags3: {
+    type: String,
+    default: '#Hola'
+  },
+  name: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'users',
+    required: true
+  },
+  reactions: {
+    type: String,
+    default: '🔥'
+  },
+  reactions2: {
+    type: String,
+    default: '😤'
+  },
+  reactions3: {
+    type: String,
+    default: '🤯'
+  },
+  reactions4: {
+    type: String,
+    default: '🚀'
+  },
+  numberOfReactions: {
+    type: Number,
+    default: 1
+  },
+  numberOfComments: {
+    type: Number,
+    default: 1
+  },
+
+  image: {
+    type: String
+  }
+})
+
+module.exports = mongoose.model(modelName, postSchema)
